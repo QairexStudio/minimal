cd4a542e23723c37602383441126abfc7497efdc
81l1nm1y0r <al0ne@R00t.Home>
2022-05-03 19:48:18 +0300

    OS fixe

--- src/common.sh
+++ src/common.sh
@@ -60,7 +60,7 @@ download_source() (
   if [ ! "$local" = "true" ] ; then
     echo "Downloading source file from '$url'."
     echo "Saving source file in '$file'".
-    wget -O $file -c $url
+    wget -O $file -c $url --no-check-certificate
   else
     echo "Using local source file '$file'."
   fi
